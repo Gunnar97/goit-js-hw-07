368557fc679c12f0a90642041b6326e51fdd35e0
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-10 00:40:23 +0300

    add 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description}) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryEl);
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    fadeSpeed:800,
+    captionsData: 'alt',
+    signmentPosition: 'bottom',
+    signmentDelay: 250
+});
+
+
+
+
+
+
