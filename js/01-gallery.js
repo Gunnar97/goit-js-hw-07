bb2897a29d9efd60c99178decad44ca90d0d4c13
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-10 00:06:38 +0300

    add 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+let instance;
+const galleryEl = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="#">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryEl);
+
+
+galleryList.addEventListener("click", onClick);
+
+
+function onClick(eve) {
+  eve.preventDefault();
+  if (!eve.target.classList.contains("gallery__image")) {
+    return;
+  }
+instance = basicLightbox.create(
+ `
+<img src="${eve.target.dataset.source}" alt="${eve.target.description}">
+`,
+    {
+        onShow: (instance) => {
+            document.addEventListener("keydown", onEsc);
+           
+        },
+        onClose: (instance) => {
+            document.removeEventListener('keydown', onEsc);
+           
+
+        }
+    }
+  );
+  instance.show();
+};
+
+function onEsc(evt) {
+    console.log(evt.key);
+    if (evt.key !== "Escape") return;
+    instance.close();
+};
+
